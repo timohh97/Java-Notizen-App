da52222e642ff6286b5ee9c05da3911e60e18292
Timo <timo.schessl@gmx.de>
2021-08-03 10:06:46 +0200

    First commit

---
+++ app/src/main/java/com/timoschessl/notizen/NoteEditorActivity.java
@@ -0,0 +1,64 @@
+package com.timoschessl.notizen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import java.util.HashSet;
+
+public class NoteEditorActivity extends AppCompatActivity {
+
+    int noteId;
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_editor);
+
+
+        EditText editText = findViewById(R.id.editText);
+        sharedPreferences = getApplicationContext().getSharedPreferences("com.timoschessl.notizen", Context.MODE_PRIVATE);
+
+        Intent intent = getIntent();
+
+        noteId = intent.getIntExtra("noteId", -1);
+
+        if(noteId!=-1)
+        {
+            editText.setText(MainActivity.notes.get(noteId));
+        }
+        else
+        {
+            MainActivity.notes.add("");
+            noteId = MainActivity.notes.size()-1;
+        }
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                MainActivity.notes.set(noteId,String.valueOf(s));
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+
+                HashSet<String> set = new HashSet<>(MainActivity.notes);
+                sharedPreferences.edit().putStringSet("notes",set).apply();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+}
